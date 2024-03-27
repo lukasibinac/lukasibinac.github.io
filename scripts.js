5dcf2a1caba4c0bcfd7dfcc843a0a008dc95c264
Luka Šibinac <157518439+lukasibinac@users.noreply.github.com>
2024-03-27 04:31:26 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,3 +10,20 @@ function changeImage() {
         additionalImages.style.display = "none"; // Hide additional images
     }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+        const gifs = document.querySelectorAll(".gif");
+
+        gifs.forEach(gif => {
+            const fullAnimationSrc = gif.getAttribute("data-src");
+            const firstFrameSrc = gif.getAttribute("src");
+
+            gif.addEventListener("mouseover", function() {
+                gif.setAttribute("src", fullAnimationSrc);
+            });
+
+            gif.addEventListener("mouseout", function() {
+                gif.setAttribute("src", firstFrameSrc);
+            });
+        });
+    });
